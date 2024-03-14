420cb098ddf7a69b3d62bf5efa03fe02acae699d
gokhangunduz <me@gokhangunduz.dev>
2024-03-14 12:17:43 +0300

    feat(help): :tada: add help frame

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -2,8 +2,9 @@ import { ReactElement } from "react";
 
 export default function Help(): ReactElement {
   return (
-    <div>
-      <h1>Help</h1>
-    </div>
+    <iframe
+      className="hw-full"
+      src="https://forms.zohopublic.eu/robolaunch/form/robolaunchPlatformForm/formperma/wdzEXselobn0zyK7bwEtjUQNAl0pR0FakvwmP1BS7mY"
+    />
   );
 }

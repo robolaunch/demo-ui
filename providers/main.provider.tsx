c72862304dcb93fae4b95eb65dd0016382aa586c
gokhangunduz <me@gokhangunduz.dev>
2024-03-25 10:48:00 +0300

    feat(version): release 0.0.9 version

--- providers/main.provider.tsx
+++ providers/main.provider.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-import "animate.css";
 import Head from "next/head";
-import "@/styles/global.css";
 import { Fragment, ReactElement } from "react";
+import MainLayout from "@/layouts/main.layout";
+import { PrimeReactProvider } from "primereact/api";
+import MainContextProvider from "@/contexts/main.context";
+import { Toaster } from "sonner";
+import Version from "@/components/Version/Version";
+import KeycloakProvider from "./keycloak.provider";
+import HiddenFrame from "@/components/HiddenFrame/HiddenFrame";
 import "@fontsource/poppins/100.css";
 import "@fontsource/poppins/200.css";
 import "@fontsource/poppins/300.css";
@@ -13,16 +18,11 @@ import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
 import "@fontsource/poppins/800.css";
 import "@fontsource/poppins/900.css";
-import MainLayout from "@/layouts/main.layout";
-import { PrimeReactProvider } from "primereact/api";
-import MainContextProvider from "@/contexts/main.context";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
-import "react-loading-skeleton/dist/skeleton.css";
-import { Toaster } from "sonner";
-import Version from "@/components/Version/Version";
-import KeycloakProvider from "./keycloak.provider";
+import "animate.css";
 import "primeicons/primeicons.css";
-import HiddenFrame from "@/components/HiddenFrame/HiddenFrame";
+import "react-loading-skeleton/dist/skeleton.css";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "@/styles/global.css";
 
 interface IMainProvider {
   children: Readonly<ReactElement | ReactElement[]>;
